ecd1ac52d20aee1c04848d224c34f98f9938586a
Josef Utbult <josef@utbult.design>
2022-11-25 00:48:11 +0100

    Add dark and light theme switching

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-Setup
+## Setup
 ```
 pip install .
 ```
+
+## TODO
+
+- [ ] Use SVG files instead of inline SVG. Figure out how to get the fill color to work properly with external files
---
+++ utbult_design_theme/static/js/base.js
@@ -0,0 +1,11 @@
+function toggleLightDarkMode(button) {
+    body = document.body
+    if(button.checked){
+        body.classList.remove('light')
+        body.classList.add('dark')
+    }
+    else {
+        body.classList.remove('dark')
+        body.classList.add('light')
+    }
+}
